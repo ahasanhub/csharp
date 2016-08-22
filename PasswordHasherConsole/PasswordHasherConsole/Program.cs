500cf41c34151334c8cd16db9f71a851127c664d
Ahasan <shobuz.cse03@gmail.com>
2016-08-22 14:27:42 +0600

    Insert password hasher console

---
+++ PasswordHasherConsole/PasswordHasherConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace PasswordHasherConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            var secrate = Guid.NewGuid();
+            var hasher = new PasswordHasher();
+            var passwordHash = hasher.HashPassword("mzaman9");
+            var sd = hasher.VerifyHashedPassword(passwordHash, "mzaman9");
+            Console.WriteLine(passwordHash);
+        }
+    }
+}
