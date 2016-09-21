7cea4c17fdcb080188a6180c801cbf549df0ef3a
Zayn <shobuz_cse03@yahoo.com>
2016-09-22 00:15:26 +0600

    update from home

--- DesignPatternConsole/Creational/GoF.Singleton.Structural/Program.cs
+++ DesignPatternConsole/Creational/GoF.Singleton.Structural/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace GoF.Singleton.Structural
 {
+    /// <summary>
+    /// Main startup class for Structural.
+    /// Singleton design pattern.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //Constractor is protected -- cannot use new
+            Singleton s1 = Singleton.Instance();
+            Singleton s2=Singleton.Instance();
+            //Test same instance
+            if (s1==s2)
+            {
+                Console.WriteLine("Objects are the same instance.");
+            }
+            //Wait for user
+            Console.ReadKey();
+        }
+    }
+    /// <summary>
+    /// The 'Singleton' class.
+    /// </summary>
+    class  Singleton
+    {
+        private static Singleton _instance;
+        //Constructor is 'protected'
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            //Uses lazy initialization.
+            //Note: This is not thread safe.
+            if (_instance==null)
+            {
+                _instance=new Singleton();
+            }
+            return _instance;
         }
     }
 }
