09d1d4c749a8b1f2acded40ef50bfea2384bad2a
Zayn <shobuz_cse03@yahoo.com>
2016-09-27 00:23:09 +0600

    Adapter pattern  from home

---
+++ DesignPatternConsole/Structural/GoF.Adapter.RealWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoF.Adapter.RealWorld
+{
+    /// <summary>
+    /// Main startup class or Real-World.
+    /// Adapter design pattarn.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Compound
+    {
+        
+    }
+}
