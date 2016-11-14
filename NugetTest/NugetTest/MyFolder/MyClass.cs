112b3183d6dccc9c9e9eb7eb5985dc843c5f2320
Zayn An Nisa <shobuz_cse03@yahoo.com>
2016-11-15 02:03:37 +0600

    Created Nuget Package demo

---
+++ NugetTest/NugetTest/MyFolder/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetDemo
+{
+    class MyClass
+    {
+        async Task DownloadAsync()
+        {
+            HttpClient client=new HttpClient();
+            await client.GetStringAsync("");
+        }
+    }
+}
