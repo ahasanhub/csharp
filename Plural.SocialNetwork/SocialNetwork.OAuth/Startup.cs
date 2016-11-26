39e76f4a96dccbdc2fe0b2ce9acdb5ae9136b2db
Zayn An Nisa <shobuz_cse03@yahoo.com>
2016-11-26 23:58:23 +0600

    update from home

--- Plural.SocialNetwork/SocialNetwork.OAuth/Startup.cs
+++ Plural.SocialNetwork/SocialNetwork.OAuth/Startup.cs
@@ -5,6 +5,13 @@ using System.Threading.Tasks;
 using IdentityServer3.Core.Configuration;
 using Microsoft.Owin;
 using Owin;
+using IdentityServer3.EntityFramework;
+using System.Collections.Generic;
+using IdentityServer3.Core.Models;
+using System.Linq;
+using System.Data.SqlClient;
+using IdentityServer3.Core.Services;
+using SocialNetwork.Data.Repositories;
 
 [assembly: OwinStartup(typeof(SocialNetwork.OAuth.Startup))]
 
@@ -14,11 +21,26 @@ namespace SocialNetwork.OAuth
     {
         public void Configuration(IAppBuilder app)
         {
+            var entityFrameworkOptions = new EntityFrameworkServiceOptions
+            {
+                ConnectionString =
+                    ConfigurationManager.ConnectionStrings["SocialNetwork.Idsvr"].ConnectionString
+            };
             var inMemoryManager = new InMemoryManager();
-            var factory = new IdentityServerServiceFactory()
-                .UseInMemoryClients(inMemoryManager.GetClients())
-                .UseInMemoryScopes(inMemoryManager.GetScopes())
-                .UseInMemoryUsers(inMemoryManager.GetUsers());
+            SetupClients(inMemoryManager.GetClients(), entityFrameworkOptions);
+            SetupScopes(inMemoryManager.GetScopes(), entityFrameworkOptions);
+            var userRepository = new Data.Repositories.UserRepository(
+                () => new SqlConnection(ConfigurationManager.ConnectionStrings["SocialNetwork"].ConnectionString)
+            );
+
+            var factory = new IdentityServerServiceFactory();
+            factory.RegisterConfigurationServices(entityFrameworkOptions);
+            factory.RegisterOperationalServices(entityFrameworkOptions);
+            factory.UserService = new Registration<IUserService>(typeof(SocialNetworkUserService));
+            factory.Register(new Registration<IUserRepository>(userRepository));
+
+            new TokenCleanup(entityFrameworkOptions, 1).Start();
+
             var options=new IdentityServerOptions
             {
                 SigningCertificate = new X509Certificate2(Convert.FromBase64String(ConfigurationManager.AppSettings["SigningCertificate"]),ConfigurationManager.AppSettings["SigningCertificatePassword"]),
@@ -28,6 +50,42 @@ namespace SocialNetwork.OAuth
             };
 
             app.UseIdentityServer(options);
+        }
+
+        public void SetupClients(IEnumerable<Client> clients,
+                                    EntityFrameworkServiceOptions options)
+        {
+            using (var context =
+                new ClientConfigurationDbContext(options.ConnectionString,
+                                                options.Schema))
+            {
+                if (context.Clients.Any()) return;
+
+                foreach (var client in clients)
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        public void SetupScopes(IEnumerable<Scope> scopes,
+                                 EntityFrameworkServiceOptions options)
+        {
+            using (var context =
+                new ScopeConfigurationDbContext(options.ConnectionString,
+                                                options.Schema))
+            {
+                if (context.Scopes.Any()) return;
+
+                foreach (var scope in scopes)
+                {
+                    context.Scopes.Add(scope.ToEntity());
+                }
+
+                context.SaveChanges();
+            }
         }
     }
 }
