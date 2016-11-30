a6d75b55986bfc9d06d345a83160d4aba97fca2b
Zayn An Nisa <shobuz_cse03@yahoo.com>
2016-12-01 01:30:29 +0600

    Update from home

---
+++ Plural.TipsTrap2/Plural.TipsTrap2/CompilerInferredGenericMethodTypes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Plural.TipsTrap2
+{
+    [TestClass]
+    public class CompilerInferredGenericMethodTypes
+    {
+        [TestMethod]
+        public void Example()
+        {
+            const string name = "Ahasan";
+            const int age = 32;
+            //DebugToWrite(age);
+            DebugToWrite<string>(name);
+            DebugToWrite<int>(age);
+
+        }
+        void DebugToWrite<T>(T obj)
+        {
+            Debug.WriteLine(obj);
+        }
+
+        void DebugToWrite(object s)
+        {
+            Debug.WriteLine(s);
+        }
+    }
+}
