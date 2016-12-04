69a0405294957ce602cdb8ee7fb24dacad8b9765
Zayn An Nisa <shobuz_cse03@yahoo.com>
2016-12-05 00:24:07 +0600

    update

--- DelegateAndEvents/DelegateAndEvents/Program.cs
+++ DelegateAndEvents/DelegateAndEvents/Program.cs
@@ -6,41 +6,59 @@ using System.Threading.Tasks;
 
 namespace DelegateAndEvents
 {
-    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    
     class Program
     {
         static void Main(string[] args)
         {
-            WorkPerformedHandler del1=new WorkPerformedHandler(WorkPerformed1);
-            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
-            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
-            //del1(5,WorkType.Golf);
-            //del2(10,WorkType.GeneratesReport);
-            //del1 += del2;
-            //del1 += del3;
-            //DoWork(del1);
-            del1 += del2 + del3;
-            del1(10,WorkType.Golf);
+            //WorkPerformedHandler del1=new WorkPerformedHandler(WorkPerformed1);
+            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
+            ////del1(5,WorkType.Golf);
+            ////del2(10,WorkType.GeneratesReport);
+            ////del1 += del2;
+            ////del1 += del3;
+            ////DoWork(del1);
+            //del1 += del2 + del3;
+            //int finalHours=del1(10,WorkType.Golf);
+            //Console.WriteLine(finalHours.ToString());
+            Worker worker=new Worker();
+            worker.WorkPerformed+=new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
+            worker.WorkCompleted+=new EventHandler(Worker_WorkPerformed);
+            worker.DoWork(8,WorkType.GeneratesReport);
             Console.ReadLine();
         }
 
-        static void DoWork(WorkPerformedHandler del)
+        private static void Worker_WorkPerformed(object sender, EventArgs e)
         {
-            del(5,WorkType.Golf);
+            Console.WriteLine("Worker is done.");
         }
 
-        static void WorkPerformed1(int hours, WorkType workType)
+        private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
         {
-            Console.WriteLine("WorkPerformed1 called "+hours.ToString());
+           Console.WriteLine("Hours worked : "+e.Hours+" "+e.WorkType);
         }
-        static void WorkPerformed2(int hours, WorkType workType)
-        {
-            Console.WriteLine("WorkPerformed2 called "+hours.ToString());
-        }
-        static void WorkPerformed3(int hours, WorkType workType)
-        {
-            Console.WriteLine("WorkPerformed3 called " + hours.ToString());
-        }
+
+        //static void DoWork(WorkPerformedHandler del)
+        //{
+        //    del(5,WorkType.Golf);
+        //}
+
+        //static int WorkPerformed1(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed1 called "+hours.ToString());
+        //    return hours + 1;
+        //}
+        //static int WorkPerformed2(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed2 called "+hours.ToString());
+        //    return hours + 1;
+        //}
+        //static int WorkPerformed3(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed3 called " + hours.ToString());
+        //    return hours + 1;
+        //}
     }
 
     public enum WorkType
