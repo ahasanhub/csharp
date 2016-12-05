b688646aecb38010f91aa22cc13f86b910a75985
Zayn An Nisa <shobuz_cse03@yahoo.com>
2016-12-06 01:20:18 +0600

    Update from home

--- Plural.LambdasAndDelegates/Plural.LambdasAndDelegates/Program.cs
+++ Plural.LambdasAndDelegates/Plural.LambdasAndDelegates/Program.cs
@@ -10,6 +10,24 @@ namespace Plural.LambdasAndDelegates
     {
         static void Main(string[] args)
         {
+            var worker = new Worker();
+            //worker.WorkPerformed += Worker_WorkPerformed;
+            //worker.WorkCompleted += Worker_WorkCompleted;
+            worker.WorkPerformed+=(s,e)=> Console.WriteLine("Worked: " + e.Hours + " hour(s) doing: " + e.WorkType);
+            worker.WorkCompleted +=(s,e)=> Console.WriteLine("Work is complete!");
+            worker.DoWork(8, WorkType.GenerateReports);
+
+            Console.Read();
         }
+
+        //static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        //{
+        //    Console.WriteLine("Worked: " + e.Hours + " hour(s) doing: " + e.WorkType);
+        //}
+
+        //static void Worker_WorkCompleted(object sender, EventArgs e)
+        //{
+        //    Console.WriteLine("Work is complete!");
+        //}
     }
 }
