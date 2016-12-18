484cf3bc5417c347c0d057e0b753bd25a11d6715
Zayn An Nisa <shobuz_cse03@yahoo.com>
2016-12-18 23:16:08 +0600

    Update from home

---
+++ TutorialsPoint.CSharp/TutorialsPoint.CSharp/EventProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TutorialsPoint.CSharp
+{
+    public delegate string MyDel(string str);
+
+    [TestClass]
+    public class EventProgram
+    {
+        private event MyDel MyEvent;
+
+        public EventProgram()
+        {
+            MyEvent+=new MyDel(WelcomeUser);
+        }
+
+        public string WelcomeUser(string username)
+        {
+            return "Welcome " + username;
+        }
+
+        [TestMethod]
+        public void MainEvent()
+        {
+            EventProgram eventProgram=new EventProgram();
+            string result = MyEvent("Tutorials Point");
+            Debug.WriteLine(result);
+        }
+    }
+}
