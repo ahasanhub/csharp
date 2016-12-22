b357f28786699c3bbe326e46d949f870f5edff27
Zayn An Nisa <shobuz_cse03@yahoo.com>
2016-12-23 01:34:43 +0600

    update from home

---
+++ Plural.BaseStreams/FileStream/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (System.IO.FileStream fs = new System.IO.FileStream(@"C:\Files\file1.txt",FileMode.Create,FileAccess.Write))
+            {
+                fs.WriteByte(100);
+                fs.Position = 0;
+                if (fs.CanRead)
+                {
+                    fs.ReadByte();
+                }
+                
+            }
+        }
+
+        private static byte[] ReadBytes(System.IO.Stream stream)
+        {
+            //dataToRead will hold the data read from the stream
+            byte[] dataToRead = new byte[stream.Length];
+            //this is the number of bytes read. This will be incremented 
+            // and eventully will equal the bytes size held by the stream
+            int totalBytesRead = 0;
+            //this is the number of bytes read in each iteration
+            int chunkBytesRead = 1;
+            while (totalBytesRead < dataToRead.Length && chunkBytesRead > 0)
+            {
+                chunkBytesRead = stream.Read(dataToRead, totalBytesRead, dataToRead.Length - totalBytesRead);
+                totalBytesRead = totalBytesRead + chunkBytesRead;
+            }
+            return dataToRead;
+        }
+    }
+}
---
+++ Plural.DecoratorStreams/Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener tcpListener = new TcpListener(ip, 5200);
+            tcpListener.Start();
+
+            int bytesReceived, totalReceived = 0;
+            byte[] receivedData = new byte[2000000];
+
+            Console.Write("Listening... ");
+
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+            NetworkStream ns = tcpClient.GetStream();
+
+            Console.WriteLine("Client connected.");
+            Console.Write("Receiving data... ");
+
+            do
+            {
+                bytesReceived = ns.Read(receivedData, 0, receivedData.Length);
+                totalReceived += bytesReceived;
+            }
+            while (bytesReceived != 0);
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
