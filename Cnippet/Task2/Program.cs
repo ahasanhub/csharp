e0bbba0255f293f2e81587d07583dd6c6eafa2d7
Zayn An Nisa <shobuz_cse03@yahoo.com>
2017-01-04 00:17:30 +0600

    Update from home

--- Cnippet/Task2/Program.cs
+++ Cnippet/Task2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task2
@@ -11,7 +12,10 @@ namespace Task2
         static Random random=new Random();
         static void Main(string[] args)
         {
-
+            //Wait on a single task with no timeout specified
+            Task taskA=Task.Run(()=>Thread.Sleep(2000));
+            Console.WriteLine($"task status : {taskA.Status}");
+            Console.ReadKey();
         }
     }
 }
---
+++ Cnippet/Task3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw=Stopwatch.StartNew();
+            Console.WriteLine("Program Begin");
+            //DoAsTask();
+            DoAsAsync();
+            Console.WriteLine("Program End");
+            Console.WriteLine("Time"+sw.ElapsedMilliseconds/1000.0);
+            Console.ReadLine();
+        }
+
+        private static void DoAsTask()
+        {
+           Console.WriteLine("1 - Starting");
+           Task<int> t=Task.Run<int>(()=>DoSomethingThatTakesTime());
+           Console.WriteLine("2 - Task started");
+           t.Wait();
+           Console.WriteLine("3 - Task completed with result : "+t.Result);
+        }
+
+        private static async void DoAsAsync()
+        {
+            Console.WriteLine("1 - Starting");
+            Task<int> t = Task.Run<int>(() => DoSomethingThatTakesTime());
+            Console.WriteLine("2 - Task started");
+            var result = await t;
+            Console.WriteLine("3 - Task completed with result : " + result);
+        }
+
+        static int DoSomethingThatTakesTime()
+        {
+            Console.WriteLine("A - Started something");
+            Thread.Sleep(2000);
+            Console.WriteLine("B - Completed something");
+            return 120;
+        }
+    }
+}
