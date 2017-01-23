6c92da1c970db05f39989816bff8ec43c74bd7b3
Zayn An Nisa <shobuz_cse03@yahoo.com>
2017-01-24 01:01:55 +0600

    update from home

---
+++ HttpClient/HttpClient/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace HttpClient
+{
+    class Program
+    {
+        static System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+
+        static void ShowProduct(Product product)
+        {
+            Console.WriteLine($"Name :{product.Name}\t Price:{product.Price}\t Category:{product.Category}");
+        }
+
+        static async Task<Uri> CreateProductAsync(Product product)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/products", product);
+            response.EnsureSuccessStatusCode();
+            return response.Headers.Location;
+        }
+
+        static async Task<Product> GetProductAsync(string path)
+        {
+            Product product = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<Product>();
+            }
+            return product;
+        }
+
+        static async Task<Product> UpdateProductAsync(Product product)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync($"api/products/{product.Id}", product);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            product = await response.Content.ReadAsAsync<Product>();
+            return product;
+        }
+
+        static async Task<HttpStatusCode> DeleteProductAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync($"api/products/{id}");
+            return response.StatusCode;
+        }
+
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("http://localhost:5843/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+           
+            try
+            {
+                //Create product
+                Product product = new Product { Name = "Pen", Price = 12, Category = "Pc" };
+                var url = await CreateProductAsync(product);
+                Console.WriteLine($"Created at {url}");
+                // Get the product
+                product = await GetProductAsync(url.PathAndQuery);
+                ShowProduct(product);
+
+                // Update the product
+                Console.WriteLine("Updating price...");
+                product.Price = 80;
+                await UpdateProductAsync(product);
+
+                // Get the updated product
+                product = await GetProductAsync(url.PathAndQuery);
+                ShowProduct(product);
+
+                // Delete the product
+                var statusCode = await DeleteProductAsync(product.Id);
+                Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
+            }
+            catch (Exception ex)
+            {
+               Console.WriteLine(ex.Message); 
+            }
+            Console.ReadLine();
+        }
+
+      
+    }
+}
