0ce3e23c3f55f8ed90d2a95507a2aaa2654e2ed8
Zayn An Nisa <shobuz_cse03@yahoo.com>
2017-01-28 18:18:38 +0600

    update from home

---
+++ ContosoUniversityMVC5EF6/ContosoUniversityMVC5EF6/Dal/SchoolContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ContosoUniversityMVC5EF6.Models;
+
+namespace ContosoUniversityMVC5EF6.Dal
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext():base("SchoolContext") { }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
