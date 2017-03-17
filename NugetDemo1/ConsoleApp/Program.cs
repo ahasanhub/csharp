b4479a60f5367be921f4aa8573a70bba4dfb7f68
Zayn An Nisa <shobuz_cse03@yahoo.com>
2017-03-17 16:59:31 +0600

    update from home

---
+++ NugetDemo1/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NugetDemo1;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath sdf=new MyMath();
+            var ss = sdf.Add(2, 4);
+            Console.WriteLine(ss);
+            Console.ReadKey();
+        }
+    }
+}
