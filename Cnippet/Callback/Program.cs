ee604b7c0245627311c61627e4811be77fb71f0f
Ahasan <shobuz.cse03@gmail.com>
2017-05-03 16:22:03 +0600

    update from office

---
+++ Cnippet/Callback/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Callback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallBackTest callBackTest=new CallBackTest();
+            callBackTest.Test();
+            Console.ReadLine();
+        }
+    }
+}
